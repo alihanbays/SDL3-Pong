4450d8a35817b247fac59fbdc4668cf0e8ed2247
Alihan Baysal <baysal.alihan@outlook.com>
2025-10-27 20:39:46 -0700

    Implemented texture rendering

--- main.cpp
+++ main.cpp
@@ -1,12 +1,93 @@
 #include <SDL3/SDL.h>
+#include <SDL3/SDL_main.h>
+#include <SDL3_image/SDL_image.h>
 #include <string>
+
+class Texture {
+    public:
+        Texture();
+        ~Texture();
+        bool loadFromFile(std::string path);
+        void destroy();
+        bool isLoaded();
+        void render(float x, float y);
+        int getHeight();
+        int getWidth();
+
+    private:
+        SDL_Texture* sdlTexture;
+        int height;
+        int width;
+};
 
 constexpr int ScreenWidth {640};
 constexpr int ScreenHeight {480};
-SDL_Window* Window {nullptr}; 
-SDL_Surface* ScreenSurface {nullptr};
-SDL_Surface* HelloWorld {nullptr};
+SDL_Window* window {nullptr}; 
+SDL_Surface* screenSurface {nullptr};
+SDL_Renderer* renderer {nullptr};
+SDL_Surface* helloWorld {nullptr};
+Texture pngTexture;
 
+Texture::Texture():
+    sdlTexture{nullptr},
+    height{0},
+    width{0}
+{
+}
+
+Texture::~Texture() {
+    destroy();
+}
+
+bool Texture::loadFromFile(std::string path) {
+      // If already exists
+    destroy();
+
+    SDL_Surface* surface = IMG_Load(path.c_str());
+    
+    if (surface == nullptr) {
+        SDL_Log("Surface could not be initilized! SDL error: %s\n", SDL_GetError());
+        return false;
+    }
+
+    sdlTexture = SDL_CreateTextureFromSurface(renderer, surface);
+
+    if (sdlTexture == nullptr) {
+        SDL_Log("Texture could not be initilized! SDL error: %s\n", SDL_GetError());
+        return false;
+    }
+
+    height = surface->h;
+    width = surface->w;
+
+    SDL_DestroySurface(surface);
+
+    return true;
+} 
+
+void Texture::destroy() {
+    SDL_DestroyTexture(sdlTexture);
+    sdlTexture = nullptr;
+    height = 0;
+    width = 0;
+}
+
+void Texture::render(float x, float y) {
+    SDL_FRect rect{x, y, static_cast<float>(height), static_cast<float>(width)};
+    SDL_RenderTexture(renderer, sdlTexture, nullptr, &rect);
+}
+
+int Texture::getHeight() {
+    return height;
+}
+
+int Texture::getWidth() {
+    return width;
+}
+
+bool Texture::isLoaded() {
+    return sdlTexture != nullptr;
+}
 
 bool init() {
     bool success {true};
@@ -14,14 +95,11 @@ bool init() {
     if (!SDL_InitSubSystem(SDL_INIT_VIDEO)) {
         SDL_Log("SDL could not be initilized! SDL error: %s\n", SDL_GetError());
         success = false;
-    } else {
-        Window = SDL_CreateWindow("Pong", ScreenWidth, ScreenHeight, 0);
+    } 
 
-        if (Window == nullptr) {
-            SDL_Log("Window could not be created! SDL error: %s\n", SDL_GetError());
-        }
-
-        ScreenSurface = SDL_GetWindowSurface(Window);
+    if (!SDL_CreateWindowAndRenderer("Pong", ScreenWidth, ScreenHeight, 0, &window, &renderer)) {
+        SDL_Log("SDL could not create window and renderer! SDL error: %s\n", SDL_GetError());
+        success = false;
     }
 
     return success;
@@ -30,24 +108,31 @@ bool init() {
 bool loadMedia() {
     bool success {true};
 
-    std::string imagePath {"./hello-sdl3.bmp"};
-
-    HelloWorld = SDL_LoadBMP(imagePath.c_str()) ;
-
-    if (HelloWorld == nullptr) {
+    if (pngTexture.loadFromFile("hello-sdl3.bmp") == false) {
         SDL_Log("Unable to load image! SDL error: %s\n", SDL_GetError());
+        success = false;
     }
+    
+    // std::string imagePath {"./hello-sdl3.bmp"};
+
+    // helloWorld = SDL_LoadBMP(imagePath.c_str()) ;
+
+    // if (helloWorld == nullptr) {
+    //     SDL_Log("Unable to load image! SDL error: %s\n", SDL_GetError());
+    // }
 
     return success;
 }
 
 void close() {
-    SDL_DestroySurface(HelloWorld);
-    HelloWorld = nullptr;
-
-    SDL_DestroyWindow(Window);
-    Window = nullptr;
-    ScreenSurface = nullptr;
+    pngTexture.destroy();
+    SDL_DestroySurface(helloWorld);
+    helloWorld = nullptr;
+    SDL_DestroyRenderer(renderer);
+    renderer = nullptr;
+    SDL_DestroyWindow(window);
+    window = nullptr;
+    screenSurface = nullptr;
 
     SDL_Quit();
 }
@@ -80,12 +165,10 @@ int main(int argc, char* args[]) {
             }
         }
 
-        uint32_t colorWhite = SDL_MapSurfaceRGB(ScreenSurface, 0xFF, 0xFF, 0xFF);
-
-        SDL_FillSurfaceRect(ScreenSurface, nullptr, colorWhite);
-        SDL_BlitSurface(HelloWorld, nullptr, ScreenSurface, nullptr);
-
-        SDL_UpdateWindowSurface(Window);
+        SDL_SetRenderDrawColor(renderer, 0xFF, 0xFF, 0xFF, 0xFF);
+        SDL_RenderClear(renderer);
+        pngTexture.render(0.f, 0.f);
+        SDL_RenderPresent(renderer);
     }
 
     close();
