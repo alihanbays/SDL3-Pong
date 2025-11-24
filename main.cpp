5a2c331f61064ce1ac29736fd268029d4cc0ae01
Alihan Baysal <baysal.alihan@outlook.com>
2025-11-24 11:59:04 -0800

    Implemented rect moving around

--- main.cpp
+++ main.cpp
@@ -5,149 +5,77 @@
 #include <string>
 #include <sstream>
 
-class Texture {
+class Box {
     public:
-        Texture();
-        ~Texture();
-        bool loadFromFile(const std::string &path);
-        void destroy();
-        void render(float x, float y, const SDL_FRect *clip, float newWidth, float newHeight) const;
-        bool createTextureFromText(const std::string &text);
-        bool isLoaded() const;
-        int getHeight() const;
-        int getWidth() const;
-        int setHeight(int newHeight);
-        int setWidth(int newWidth);
-
+        Box();
+        int boxHeight {20};
+        int boxWidth {20};
+        int maxVelocity {5};
+        void move();
+        void render();
+        void handleEvent(SDL_Event &event);
     private:
-        SDL_Texture* sdlTexture;
-        int height;
-        int width;
+        SDL_Rect rect;
+        int xVelocity;
+        int yVelocity;
 };
 
 constexpr int ScreenWidth {640};
 constexpr int ScreenHeight {480};
 SDL_Window* window {nullptr};
 SDL_Renderer* renderer {nullptr};
-Texture myTexture;
+Box myBox;
 constexpr SDL_Color bgColor {0, 0, 0, 255};
 Uint64 startTime {0};
 std::stringstream timeText;
 TTF_Font *font {nullptr};
+constexpr int capFps = 60;
 
-
-Texture::Texture():
-    sdlTexture{nullptr},
-    height{0},
-    width{0}
-{
+Box::Box() {
+    SDL_Rect rect { 0, 0, boxWidth, boxHeight};
+    xVelocity = 0;
+    yVelocity = 0;
 }
 
-Texture::~Texture() {
-    destroy();
-}
-
-bool Texture::loadFromFile(const std::string &path) {
-    destroy();
-
-    SDL_Surface* surface = IMG_Load(path.c_str());
-
-    if (surface == nullptr) {
-        SDL_Log("Surface could not be initialized! SDL error: %s\n", SDL_GetError());
-        return false;
+void Box::handleEvent(SDL_Event &event) {
+    // -Y is up, -X is left
+    if (event.type == SDL_EVENT_KEY_DOWN && event.key.repeat == 0) {
+        switch (event.key.key) {
+            case SDLK_UP: yVelocity -= maxVelocity; break;
+            case SDLK_DOWN: yVelocity += maxVelocity; break;
+            case SDLK_LEFT: xVelocity -= maxVelocity; break;
+            case SDLK_RIGHT: xVelocity += maxVelocity; break;
+        }
     }
 
-    const bool trans = SDL_SetSurfaceColorKey(surface, true, SDL_MapSurfaceRGB(surface, 0x00, 0xFF, 0xFF));
-    if (!trans) {
-        SDL_Log("Color key could not be initialized! SDL error: %s\n", SDL_GetError());
-        return false;
+    if (event.type == SDL_EVENT_KEY_UP && event.key.repeat == 0) {
+        switch (event.key.key) {
+            case SDLK_UP: yVelocity += maxVelocity; break;
+            case SDLK_DOWN: yVelocity -= maxVelocity; break;
+            case SDLK_LEFT: xVelocity += maxVelocity; break;
+            case SDLK_RIGHT: xVelocity -= maxVelocity; break;
+        }
     }
-    sdlTexture = SDL_CreateTextureFromSurface(renderer, surface);
-
-    if (sdlTexture == nullptr) {
-        SDL_Log("Texture could not be initialized! SDL error: %s\n", SDL_GetError());
-        return false;
-    }
-
-    height = surface->h;
-    width = surface->w;
-
-    SDL_DestroySurface(surface);
-
-    return true;
 }
 
-void Texture::destroy() {
-    SDL_DestroyTexture(sdlTexture);
-    sdlTexture = nullptr;
-    height = 0;
-    width = 0;
+void Box::move() {
+    rect.x += xVelocity;
+    rect.y += yVelocity;
+
+    if (rect.x < 0) rect.x = 0;
+    if (rect.y < 0) rect.y = 0;
+
+    // 620 + 20 | 460 + 20 = 640, 480
+    if (rect.x + boxHeight > ScreenWidth) rect.x -= maxVelocity;
+    if (rect.y + boxHeight > ScreenHeight) rect.y -= maxVelocity;
+
+    SDL_Log("X: %d, Y: %d", rect.x, rect.y);
 }
 
-void Texture::render(const float x, const float y, const SDL_FRect *clip = nullptr, const float newWidth = 0, const float newHeight = 0) const {
-
-    // Construct the initial rect
-    SDL_FRect dRect{x, y, static_cast<float>(width),  static_cast<float>(height)};
-
-    if (clip != nullptr) {
-        dRect.w = clip->w;
-        dRect.h = clip->h;
-    }
-
-    if (newWidth > 0) {
-        dRect.w = newWidth;
-    }
-    if (newHeight > 0) {
-        dRect.h = newHeight;
-    }
-
-    if (sdlTexture == nullptr) {
-        SDL_Log("WTF bro");
-    }
-    SDL_RenderTexture(renderer, sdlTexture, clip, &dRect);
-}
-
-bool Texture::createTextureFromText(const std::string &text) {
-    destroy();
-    SDL_Color white = {255, 255, 255, 255};
-    SDL_Surface *textSurface = TTF_RenderText_Solid(font, text.c_str(), text.size(), white);
-    if (textSurface == nullptr) {
-        SDL_Log("Text surface can not be initialized! SDL error: %s\n", SDL_GetError());
-        return false;
-    }
-
-    sdlTexture = SDL_CreateTextureFromSurface(renderer, textSurface);
-
-    if (sdlTexture == nullptr) {
-        SDL_Log("Texture could not be initialized! SDL error: %s\n", SDL_GetError());
-        return false;
-    }
-
-    width = textSurface->w;
-    height = textSurface->h;
-
-    SDL_DestroySurface(textSurface);
-    return true;
-}
-
-int Texture::getHeight() const {
-    return height;
-}
-
-int Texture::getWidth() const {
-    return width;
-}
-
-int Texture::setHeight(const int newHeight) {
-    return height = newHeight;
-}
-
-int Texture::setWidth(const int newWidth) {
-    return width = newWidth;
-}
-
-bool Texture::isLoaded() const {
-    return sdlTexture != nullptr;
+void Box::render() {
+    SDL_FRect drawRect { static_cast<float>(rect.x), static_cast<float>(rect.y), static_cast<float>(boxWidth), static_cast<float>(boxHeight)};
+    SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
+    SDL_RenderRect(renderer, &drawRect);
 }
 
 bool init() {
@@ -174,12 +102,17 @@ bool init() {
 bool loadMedia() {
     bool success {true};
 
-    std::string fontPath {"./DirektorCondensed.ttf"};
-    font = TTF_OpenFont( fontPath.c_str(), 30 );
-    if (font == nullptr){
-        SDL_Log( "Could not load %s! SDL_ttf Error: %s\n", fontPath.c_str(), SDL_GetError() );
-        success = false;
-    }
+    // std::string fontPath {"./DirektorCondensed.ttf"};
+    // font = TTF_OpenFont( fontPath.c_str(), 30 );
+    // if (font == nullptr){
+    //     SDL_Log( "Could not load %s! SDL_ttf Error: %s\n", fontPath.c_str(), SDL_GetError() );
+    //     success = false;
+    // }
+
+    // if (myTexture.loadFromFile("dots.png") == false) {
+    //     SDL_Log("Unable to load image! SDL error: %s\n", SDL_GetError());
+    //     success = false;
+    // }
     return success;
 }
 
@@ -208,19 +141,18 @@ int main(int argc, char* args[]) {
             if (event.type == SDL_EVENT_QUIT) {
                 quit = true;
             }
+            myBox.handleEvent(event);
         }
+        myBox.move();
         SDL_SetRenderDrawColor(renderer, bgColor.r, bgColor.g, bgColor.b, bgColor.a);
         SDL_RenderClear(renderer);
-
-        if (event.type == SDL_EVENT_KEY_DOWN) {
-            if (event.key.key == SDLK_SPACE) {
-                startTime = SDL_GetTicks();
-            }
-        }
-        std::string timePassed  = std::to_string((SDL_GetTicks() - startTime) / 1000);
-        myTexture.createTextureFromText(timePassed);
-        myTexture.render((ScreenWidth - myTexture.getWidth()) / 2.f, (ScreenHeight - myTexture.getHeight()) / 2.f);
+        myBox.render();
         SDL_RenderPresent(renderer);
+
+        constexpr Uint64 nsPerFrame = 1000000000 / capFps;
+        if (const Uint64 frameNs {SDL_GetTicksNS()}; frameNs < nsPerFrame) {
+            SDL_Delay(nsPerFrame - frameNs);
+        }
     }
 
     close();
